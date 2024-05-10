2810f3df746c7d2310443591dde36ab472d14730
Diogo Santos <diogo.magalhaescalvo@gmail.com>
2024-05-10 12:02:38 +0100

    pre final version

--- new_pipe/srcs/lib/ft_strcmp.c
+++ new_pipe/srcs/lib/ft_strcmp.c
@@ -17,8 +17,6 @@ int	ft_strcmp(const char *s1, const char *s2, size_t n)
 	size_t	c;
 
 	c = 0;
-	if (n == 0)
-		return (0);
 	while (s1[c] == s2[c] && (s1[c] && s2[c]) && c < n - 1)
 		c++;
 	return ((unsigned char)s1[c] - (unsigned char)s2[c]);
--- new_pipe/srcs/parser/here_doc.c
+++ new_pipe/srcs/parser/here_doc.c
@@ -41,7 +41,8 @@ void	here_doc(char *dli, t_pipex *pipx, int pipe_nbr)
 			write(1, "pipe ", 5);
 		write(1, "here_doc> ", 10);
 		line = get_next_line(0);
-		if (line == NULL || ft_strncmp(line, dli, ft_strlen(line) - 1) == 0)
+		printf("line: :%s:", line);
+		if (line == NULL || ft_strcmp(line, dli, ft_strlen(line) - 1) == 0)
 			break ;
 		ft_putstr_fd(line, file);
 		free(line);
@@ -52,3 +53,29 @@ void	here_doc(char *dli, t_pipex *pipx, int pipe_nbr)
 	if (pipx->infile < 0)
 		finish_here(pipx);
 }
+
+/* void	here_doc(char *dli, t_pipex *pipx, int pipe_nbr) {
+	int		file;
+	char	*line;
+
+	file = open(".heredoc_tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (file == -1)
+		error_msg("Error opening here_doc");
+	while (1) {
+		ft_putstr_fd("here_doc> ", 1); // Print prompt
+		line = get_next_line(0); // Read input line
+		if (!line || line[0] == '\0') { // Check for empty line or EOF
+			free(line);
+			break; // Exit loop
+		}
+		if (strcmp(line, dli) == 0) // Compare with limiter
+			break; // Exit loop if limiter is encountered
+		ft_putstr_fd(line, file); // Write line to file
+		free(line);
+	}
+	free(line);
+	close(file);
+	pipx->infile = open(".heredoc_tmp", O_RDONLY);
+	if (pipx->infile < 0)
+		finish_here(pipx);
+} */
