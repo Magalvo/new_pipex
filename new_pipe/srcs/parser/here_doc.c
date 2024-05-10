4b9527c0f060f1ecd201725b7771e86fe4923e1f
Magalvo <diogo.magalhaescalvo@gmail.com>
2024-05-10 12:36:05 +0100

    improved and finished

--- new_pipe/srcs/parser/here_doc.c
+++ new_pipe/srcs/parser/here_doc.c
@@ -38,10 +38,9 @@ void	here_doc(char *dli, t_pipex *pipx, int pipe_nbr)
 	{
 		i = -1;
 		while (++i < pipe_nbr)
-			write(1, "pipe ", 5);
-		write(1, "here_doc> ", 10);
+			ft_putstr_fd("pipe ", 1);
+		ft_putstr_fd("here_doc> ", 1);
 		line = get_next_line(0);
-		printf("line: :%s:", line);
 		if (line == NULL || ft_strcmp(line, dli, ft_strlen(line) - 1) == 0)
 			break ;
 		ft_putstr_fd(line, file);
@@ -53,29 +52,3 @@ void	here_doc(char *dli, t_pipex *pipx, int pipe_nbr)
 	if (pipx->infile < 0)
 		finish_here(pipx);
 }
-
-/* void	here_doc(char *dli, t_pipex *pipx, int pipe_nbr) {
-	int		file;
-	char	*line;
-
-	file = open(".heredoc_tmp", O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (file == -1)
-		error_msg("Error opening here_doc");
-	while (1) {
-		ft_putstr_fd("here_doc> ", 1); // Print prompt
-		line = get_next_line(0); // Read input line
-		if (!line || line[0] == '\0') { // Check for empty line or EOF
-			free(line);
-			break; // Exit loop
-		}
-		if (strcmp(line, dli) == 0) // Compare with limiter
-			break; // Exit loop if limiter is encountered
-		ft_putstr_fd(line, file); // Write line to file
-		free(line);
-	}
-	free(line);
-	close(file);
-	pipx->infile = open(".heredoc_tmp", O_RDONLY);
-	if (pipx->infile < 0)
-		finish_here(pipx);
-} */
